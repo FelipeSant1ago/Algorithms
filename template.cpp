0a43a01def12dbe76e2e9fd5592a11a7260e659a
FelipeSant1ago <felipes4ntiago@ufpi.edu.br>
2023-06-12 15:35:00 -0300

    updated

--- template.cpp
+++ template.cpp
@@ -2,9 +2,14 @@
 #define ll long long
 #define vi vector<int>
 #define vii vector<vector<int>>
+#define optimize ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+
 using namespace std;
 
 
+
 int main(){
+
+    optimize;
 
 }
