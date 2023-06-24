331513c006b062d2144dbade8706b5dc661a330c
FelipeSant1ago <felipes4ntiago@ufpi.edu.br>
2023-06-24 08:44:16 -0300

    add #define pii pair<int,int>

--- template.cpp
+++ template.cpp
@@ -1,8 +1,10 @@
 #include<bits/stdc++.h>
 #define ll long long
 #define vi vector<int>
-#define vii vector<vector<int>>
+#define vvi vector<vector<int>>
+#define pii pair<int,int>
 #define optimize ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+
 
 using namespace std;
 
