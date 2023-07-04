6ba8ef37a15994450b35d232f9991c7496d4ab32
FelipeSant1ago <felipes4ntiago@ufpi.edu.br>
2023-07-04 11:41:40 -0300

    updated

--- template.cpp
+++ template.cpp
@@ -1,11 +1,12 @@
 #include<bits/stdc++.h>
+#define optimize ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
 #define ll long long
 #define vi vector<int>
 #define vvi vector<vector<int>>
 #define pii pair<int,int>
-#define optimize ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
-
-
+#define MAXN 1000000
+#define INF 1000000007
+#define MOD 1000000007
 using namespace std;
 
 
